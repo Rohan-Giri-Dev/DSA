542c1d567de5b4133260fe67b4e0f15cb54b9a83
Rohan Giri <giri.dev.rohan@gmail.com>
2025-06-28 00:29:44 +0530

    Add bubble sort

---
+++ bubble_sort.cpp
@@ -0,0 +1,39 @@
+// Bubble sort
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for(int i= n-1; i >= 1; i--)
+    {
+        for(int j=0; j < i; j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void print_array(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+}
+
+int main()
+{
+    int arr[] = { 9,4,7,2,5,1,3,8,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    print_array(arr, n);
+    bubbleSort(arr,n);
+    print_array(arr, n);
+
+}
