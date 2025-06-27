a00a3894ab3b1d7de539275a26a13008b56e48b8
Rohan Giri <giri.dev.rohan@gmail.com>
2025-06-28 01:10:09 +0530

    Add didSwap in buuble sort

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -6,6 +6,7 @@ void bubbleSort(int arr[], int n)
 {
     for(int i= n-1; i >= 1; i--)
     {
+        int didSwap = 0;
         for(int j=0; j < i; j++)
         {
             if(arr[j] > arr[j+1])
@@ -13,8 +14,12 @@ void bubbleSort(int arr[], int n)
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                didSwap = 1;
             }
         }
+        if(didSwap == 0) break;
+        cout<<"Swap"<<endl;
+
     }
 }
 
