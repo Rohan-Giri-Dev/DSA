6c6a8fc44ef67b78e40eb488694dbb92f56f410e
Rohan Giri <giri.dev.rohan@gmail.com>
2025-07-07 07:03:44 +0530

    Add Rotate array by D places

---
+++ Array/Part1/arrRotateByD.cpp
@@ -0,0 +1,43 @@
+//Rotate array by D places
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void arrRotate(vector<int> &arr,int d)
+{
+    int n = arr.size();
+    vector<int> temp;
+    for(int i =0 ; i<d; i++)
+    {
+        temp.push_back(arr[i]);
+    }
+
+    for(int i=d; i<n; i++)
+    {
+        arr[i-d] = arr[i];
+    }
+
+    for(int i=n-d; i<n; i++)
+    {
+        arr[i] = temp[i-(n-d)];
+    }
+
+    
+}
+
+void printArr(vector<int> &arr)
+{
+    for(auto i = arr.begin() ; i != arr.end(); i++)
+    {
+        cout<<*i<<" ";
+    }
+}
+
+int main()
+{
+    vector<int> arr = {1,2,3,4,5};
+    printArr(arr);
+    cout<<endl;
+    arrRotate(arr,3);
+    printArr(arr);
+}
---
+++ Array/Part1/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+    // int j=0;
+    // for(int i=0; i<d; i++)
+    // {
+    //     arr[n-d] = temp[j];
+    //     j++;
+    // }
