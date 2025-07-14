5b19de1ab13edcbf3f0566bcbd03693838b863bd
Rohan Giri <giri.dev.rohan@gmail.com>
2025-07-14 10:46:40 +0530

    Add twoSum problem file

--- Array/Part2/twoSum.cpp
+++ Array/Part2/twoSum.cpp
@@ -6,17 +6,15 @@ using namespace std;
 vector<int> twoSum(vector<int>& arr, int target) {
     int n = arr.size();
     vector<int> temp;
-    int sum = 0;
 
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n ;i++)
     {
         for(int j=0; j<n; j++)
         {
-            sum = arr[i] + arr[j];
-            if(sum == target)
+            if(i == j) continue;
+            if(arr[i]+arr[j] == target)
             {
-                temp.push_back(arr[i]);
-                // temp.push_back(arr[j]);
+                temp.push_back(i);
             }
         }
     }
@@ -27,9 +25,16 @@ vector<int> twoSum(vector<int>& arr, int target) {
 int main()
 {
     vector<int> arr = {2,7,11,15};
-    vector<int> res = twoSum(arr,9);
+    vector<int> arr1 = {3,2,4};
+    vector<int> res = twoSum(arr,9); 
+    vector<int> res1 = twoSum(arr1,6);
 
     for(auto it : res)
+    {
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    for(auto it : res1)
     {
         cout<<it<<" ";
     }
