be2b2aabcc910d680c12e8d6d356e0ccab40750c
Rohan Giri <giri.dev.rohan@gmail.com>
2025-07-25 17:38:24 +0530

    Commit

--- Array/Part2/maximum_subarray.cpp
+++ Array/Part2/maximum_subarray.cpp
@@ -6,14 +6,19 @@ using namespace std;
 int maxSubArray(vector<int>& arr) {
     long long maxi = INT_MIN;
     int sum = 0;
+    int ansStart = -1, ansEnd = -1;
 
     for(int i=0; i<arr.size(); i++)
     {
+        int start;
+        if(sum == 0) start = i;
         sum += arr[i];
 
         if(sum > maxi)
         {
             maxi = sum ;
+            ansStart = start;
+            ansEnd = i;
         }
 
         if(sum < 0)
---
+++ Array/Part2/rearrangeArray.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> rearrangeArray(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> ps;
+    vector<int> ne;
+    vector<int> temp;
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i] >= 0)
+        {
+            ps.push_back(arr[i]);
+        }
+        else{
+            ne.push_back(arr[i]);
+        }
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        if( i%2 == 0)
+        {
+            temp.push_back(ps[i]);
+        }
+        else{
+            temp.push_back(ne[i]);
+        }
+    }
+
+    return temp;
+        
+}
+
+int main()
+{
+    vector<int> arr = {3,1,-2,-5,2,-4};
+    vector<int> res = rearrangeArray(arr);
+
+    for(auto it: res)
+    {
+        cout<<it;
+    }
+}
