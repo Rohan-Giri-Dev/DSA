50fda28f48069ed6d910c4bcc3efd107d0838343
Rohan Giri <giri.dev.rohan@gmail.com>
2025-07-27 00:38:38 +0530

    Add rearrange array better solution

--- Array/Part2/rearrangeArray.cpp
+++ Array/Part2/rearrangeArray.cpp
@@ -3,32 +3,52 @@
 
 using namespace std;
 
+// vector<int> rearrangeArray(vector<int>& arr) {
+//     int n = arr.size();
+//     vector<int> ps;
+//     vector<int> ne;
+//     vector<int> temp;
+
+//     for(int i=0; i<n; i++)
+//     {
+//         if(arr[i] >= 0)
+//         {
+//             ps.push_back(arr[i]);
+//         }
+//         else{
+//             ne.push_back(arr[i]);
+//         }
+//     }
+//     int pos =0 , neg = 0;
+
+//     for(int i=0; i<n; i++)
+//     {
+//         if( i%2 == 0)
+//         {
+//             temp.push_back(ps[pos++]);
+//         }
+//         else{
+//             temp.push_back(ne[neg++]);
+//         }
+//     }
+
+//     return temp;
+// }
 vector<int> rearrangeArray(vector<int>& arr) {
     int n = arr.size();
-    vector<int> ps;
-    vector<int> ne;
     vector<int> temp;
+    int posIndex = 0 , negIndex = 0;
 
     for(int i=0; i<n; i++)
     {
-        if(arr[i] >= 0)
+        if(arr[i] < 0)
         {
-            ps.push_back(arr[i]);
+            temp[negIndex] = arr[i];
+            negIndex += 2;
         }
         else{
-            ne.push_back(arr[i]);
-        }
-    }
-    int pos =0 , neg = 0;
-
-    for(int i=0; i<n; i++)
-    {
-        if( i%2 == 0)
-        {
-            temp.push_back(ps[pos++]);
-        }
-        else{
-            temp.push_back(ne[neg++]);
+            temp[posIndex] = arr[i];
+            posIndex += 2;
         }
     }
 
