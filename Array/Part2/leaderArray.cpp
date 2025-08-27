88c8a36b31b306e4fcf6f4eea3bc08f6a2b0601f
Rohan Giri <giri.dev.rohan@gmail.com>
2025-08-28 01:45:41 +0530

    Add leader array

--- Array/Part2/leaderArray.cpp
+++ Array/Part2/leaderArray.cpp
@@ -1,36 +1,38 @@
+/*Problem Statement: Given an array, print all the elements which are leaders.
+A Leader is an element that is greater than all of the elements on its right side in the array.*/
+
 #include <iostream>
+#include <bits/stdc++.h>
 #include <vector>
 using namespace std;
 
 vector<int> leadArray(vector<int> arr){
 
     int n =  arr.size();
-    vector<int> temp;
+    vector<int> res;
 
-    int sum = 0;
+    int maxRight = arr.back();
+    res.push_back(arr.back());
 
-    for(int i=0; i<n; i++)
-    {
-        if(sum<0) break;
-
-        for(int i=0; i<n; i++)
-        {
-            sum += arr[i];
-
-            temp.push_back(arr[i]);
+    for(int i=n-2; i>=0; i--){
+        if(arr[i] >= maxRight ){
+            res.push_back(arr[i]);
+            maxRight = arr[i];
         }
     }
 
-    return temp;
+    reverse(res.begin(), res.end());
+
+    return res;
 }
 
 int main()
 {
-    vector<int> arr ={4, 7, 1, 0};
+    vector<int> arr ={4,5,7,1};
 
     vector<int> ans = leadArray(arr);
 
-    for(auto it : arr)
+    for(auto it : ans)
     {
         cout<<it<<" ";
     }
