74a9d11968b8c553b579a8aef213c66252f5a842
Rohan Giri <giri.dev.rohan@gmail.com>
2025-10-25 16:22:35 +0530

    Majority Element

---
+++ Array/Part3/majorityElement.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+
+        map<int,int> mp;
+
+        for(auto num : nums){
+            mp[num]++;
+        }
+
+        for(auto it: mp){
+            if(it.second() > n){
+                ans.push_back(it.first());
+            }
+            else{
+                return nums;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    vector<int> arr = {1,2,2,2,1,1,1};
+    Solution s1 ;
+    vector<int> ans = s1.majorityElement(arr);
+
+    for(auto it: ans){
+        cout<<it;
+    }
+}
