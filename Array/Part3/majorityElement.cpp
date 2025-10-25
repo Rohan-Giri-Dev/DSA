5e192b50fd0a29b38aca0e40e5eeb1aed3877b0b
Rohan Giri <giri.dev.rohan@gmail.com>
2025-10-25 21:34:25 +0530

    Majority Element

--- Array/Part3/majorityElement.cpp
+++ Array/Part3/majorityElement.cpp
@@ -16,20 +16,21 @@ public:
         }
 
         for(auto it: mp){
-            if(it.second > n){
+            if(it.second > n/3){
                 ans.push_back(it.first);
             }
+
         }
         return ans;
     }
 };
 
 int main(){
-    vector<int> arr = {1,2,2,2,1};
+    vector<int> arr = {1,2,2,2,1,3,4,5,5,6,1,2,2,2};
     Solution s1 ;
     vector<int> ans = s1.majorityElement(arr);
 
     for(auto it: ans){
-        cout<<it;
+        cout<<it<<" ";
     }
 }
