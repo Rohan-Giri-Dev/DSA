30c0d3cffc72e050aab536f98cb883b843bf31ea
Rohan Giri <giri.dev.rohan@gmail.com>
2025-10-26 17:32:41 +0530

    3 Sum problem optimal  approach

--- Array/Part3/3sum.cpp
+++ Array/Part3/3sum.cpp
@@ -29,6 +29,8 @@ public:
                     ans.push_back(temp);
                     j++;
                     k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
                 }
             }
         }
