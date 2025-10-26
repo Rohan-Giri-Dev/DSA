838966a82eb09d7ef401cb6138cdb5efb89f8eda
Rohan Giri <giri.dev.rohan@gmail.com>
2025-10-26 17:29:12 +0530

    3 Sum problem optimal  approach

--- Array/Part3/3sum.cpp
+++ Array/Part3/3sum.cpp
@@ -8,23 +8,30 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        set<vector<int>> st;
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
 
-        for(int i = 0; i<n; i++){
-            set<int> hashset;
-            for(int j = i+1; j<n; j++){
-                    int third = -(nums[i] + nums[j]);
-                    if(hashset.find(third) != hashset.end())
-                    {
-                        vector<int> temp = {nums[i], nums[j], third};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
-                    }
-                        hashset.insert(nums[j]);
-                    }
+        for(int i=0; i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
                 }
-
-        vector<vector<int>> ans(st.begin(), st.end());
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    vector<int> temp = {nums[i],nums[j],nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                }
+            }
+        }
         return ans;
     }
 };
@@ -65,6 +72,35 @@ public:
         vector<vector<int>> ans(st.begin(), st.end());
         return ans;
 
+    }
+};
+*/
+
+/*
+Better solution
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> st;
+
+        for(int i = 0; i<n; i++){
+            set<int> hashset;
+            for(int j = i+1; j<n; j++){
+                    int third = -(nums[i] + nums[j]);
+                    if(hashset.find(third) != hashset.end())
+                    {
+                        vector<int> temp = {nums[i], nums[j], third};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                        hashset.insert(nums[j]);
+                    }
+                }
+
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
     }
 };
 */
