471c6070fb4c4887406dcb5170848cf4a76230bc
Rohan Giri <giri.dev.rohan@gmail.com>
2025-10-31 03:52:03 +0530

    4 Sum problem better

--- Array/Part3/4sum.cpp
+++ Array/Part3/4sum.cpp
@@ -11,6 +11,53 @@ public:
         set<vector<int>> st;
         for(int i=0; i<n; i++){
             for(int j= i+1; j<n; j++){
+                set<long long> hashset;
+                for(int k=j+1; k<n; k++){
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long long fourth = target - sum;
+
+                    if(hashset.find(fourth) != hashset.end()){
+                        vector<int> temp = {nums[i], nums[j],nums[k], int(fourth)};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+
+                    hashset.insert(nums[k]);
+            }
+        }
+    }
+
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
+
+int main(){
+    Solution s1;
+    vector<int> arr = {1,0,-1,0,-2,2};
+
+    vector<vector<int>> ans = s1.fourSum(arr,0);
+    for(auto &row : ans){
+        for(auto col : row){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+
+
+/*
+Brute force approach
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>> st;
+        for(int i=0; i<n; i++){
+            for(int j= i+1; j<n; j++){
                 for(int k=j+1; k<n; k++){
                     for(int l=k+1; l<n; l++){
                         long long sum = nums[i]+nums[j];
@@ -31,16 +78,4 @@ public:
     }
 };
 
-int main(){
-    Solution s1;
-    vector<int> arr = {1,0,-1,0,-2,2};
-
-    vector<vector<int>> ans = s1.fourSum(arr,0);
-    for(auto &row : ans){
-        for(auto col : row){
-            cout<<col<<" ";
-        }
-        cout<<endl;
-    }
-
-}
+*/
