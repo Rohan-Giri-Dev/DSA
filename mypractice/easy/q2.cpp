b3080b804a5a49b7d12f6af9d4b5f8b54ca95328
Rohan Giri <giri.dev.rohan@gmail.com>
2025-12-26 22:58:20 +0530

    Add mypracticr q2

--- mypractice/easy/q2.cpp
+++ mypractice/easy/q2.cpp
@@ -4,9 +4,57 @@
 #include <algorithm>
 using namespace std;
 
-void solution(vector<int> arr)
+void solution(vector<int> arr,int &secondMin, int &secondMax)
 {
     int n = arr.size();
+    // smallest so far
+    long long mn = INT_MAX;
+    long long smn = INT_MAX;
+
+    // largest
+    long long mx = INT_MIN;
+    long long smx = INT_MIN;
+
+    for(int x: arr){
+        // x is smaller then mn
+        // so 2nd smallest(smn) becomes the smallest(mn)
+        // and smallest becmes the x
+        if( x < mn){
+            smn = mn;
+            mn = x;
+        }
+        //x not euqal mn and x small then 2nd smallest
+        else if(x != mn && x < smn){
+            smn = x;
+        }
+
+        // x is grater then mx(greatest)
+        if(x > mx){
+            smx = mx;
+            mx = x;
+        }
+        // x not equals greatest(mx) && x greater then 2nd greatest(smx)
+        else if( x != mx && x > smx){
+            smx = x;
+        }
+
+        if(smn == LLONG_MAX || smx == LLONG_MIN) return;
+
+        secondMin = int(smn);
+        secondMax = int(smx);
+    }
+    cout<<"Second smallest: "<<smn<<endl;
+    cout<<"Second largest: "<<smx;
+}
+
+int main(){
+    vector<int> arr = {1, 2, 4, 7, 7, 5};
+    int secondSmallest, secondLargest;
+    solution(arr, secondSmallest, secondLargest);
+}
+
+
+/* int n = arr.size();
     // sort(arr.begin(), arr.end());
 
     // int smallest = arr[0];
@@ -34,11 +82,4 @@ void solution(vector<int> arr)
     int ansLastSmallest = lastSecond->first;
 
     cout<<"Second smallest: " << ansSmallest<<'\n';
-    cout<<"Second largest : " << ansLastSmallest;
-
-}
-
-int main(){
-    vector<int> arr = {1, 2, 4, 7, 7, 5};
-    solution(arr);
-}
+    cout<<"Second largest : " << ansLastSmallest;*/
