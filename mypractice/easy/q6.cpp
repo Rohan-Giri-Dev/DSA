b95391095080a1b02cd006f77e9582e3ad730672
Rohan Giri <giri.dev.rohan@gmail.com>
2026-01-03 15:20:25 +0530

    add move non zero elements to the right

--- mypractice/easy/q6.cpp
+++ mypractice/easy/q6.cpp
@@ -3,30 +3,30 @@
 #include <algorithm>
 using namespace std;
 
-void moveZeros(vector<int> &arr){
-    int n = arr.size();
-    vector<int> temp;
-    int count = 0;
 
-    for(int i=0; i<=n-1; i++){
-        if(arr[i] != 0){
-            temp.push_back(arr[i]);
-        }else{
-            count++;
-        }
-    }
-
-    temp.insert(temp.end(), count, 0);
-
-    // arr = temp;
-
-    for(auto &x : temp){
-        cout<<x << " ";
+        void moveZeroes(vector<int> & nums){
+            int lastNonZeroFoundAt = 0;
+// If the current element is not 0, then we need to
+// append it just in front of last non 0 element we found.
+for (int i = 0; i < nums.size(); i++)
+{
+    if (nums[i] != 0)
+    {
+        nums[lastNonZeroFoundAt++] = nums[i];
     }
 }
+// After we have finished processing new elements,
+// all the non-zero elements are already at beginning of array.
+// We just need to fill remaining elements in the array with 0's.
+for (int i = lastNonZeroFoundAt; i < nums.size(); i++)
+{
+    nums[i] = 0;
+}
+}
+
 
 int main()
 {
     vector<int> arr = {0, 1, 0, 3, 12};
-    moveZeros(arr);
+    moveZeroes(arr);
 }
