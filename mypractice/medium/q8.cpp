f4c5a344f3b59fbdb154675f5c8038637d352f2f
Rohan Giri <giri.dev.rohan@gmail.com>
2026-05-20 12:49:51 +0530

    optimal appraoch

--- mypractice/medium/q8.cpp
+++ mypractice/medium/q8.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
 #include <algorithm>
-#include <climits>
 #include <vector>
-#include <set>
+#include <unordered_set>
 using namespace std;
 
 int longestConsecutive(vector<int> &arr)
 {
     int n = arr.size();
-    sort(arr.begin(), arr.end());
+
+    if (n == 0)
+        return 0;
+
+    unordered_set<int> st;
+    int longest = 1;
+
+    for (int i = 0; i <= n - 1; i++)
+    {
+        st.insert(arr[i]);
+    }
+
+    for (auto it : st)
+    {
+        if (st.find(it - 1) == st.end())
+        {
+            int x = it;
+            int count = 1;
+
+            while (st.find(x + 1) != st.end())
+            {
+                x = x + 1;
+                count++;
+            }
+
+            longest = max(longest, count);
+        }
+    }
+
+    return longest;
+}
+
+int main()
+{
+    // vector<int> a = {100, 4, 200, 1, 3, 2};
+    vector<int> a = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+
+    // Function call for longest consecutive sequence
+    int ans = longestConsecutive(a);
+    cout << "The longest consecutive sequence is " << ans << "\n";
+    return 0;
+}
+
+/*
+better approach
+
+sort(arr.begin(), arr.end());
 
     int lastSmaller = INT_MIN;
     int longest = 1;
@@ -35,15 +80,4 @@ int longestConsecutive(vector<int> &arr)
     }
 
     return longest;
-}
-
-int main()
-{
-    // vector<int> a = {100, 4, 200, 1, 3, 2};
-    vector<int> a = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
-
-    // Function call for longest consecutive sequence
-    int ans = longestConsecutive(a);
-    cout << "The longest consecutive sequence is " << ans << "\n";
-    return 0;
-}
+*/
