c37dba1c3543b7e58fd5eb4c2a132a14c7cb99bf
Rohan Giri <giri.dev.rohan@gmail.com>
2026-08-31 04:02:31 +0530

    solved: leetcode 1544. Make The String Great

---
+++ Strings/q7.cpp
@@ -0,0 +1,27 @@
+// 1544. Make The String Great
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string makeGood(string s)
+    {
+        string result = "";
+
+        for (char &ch : s)
+        {
+            if (result.size() > 0 && ((result.back() + 32 == ch) || (result.back() - 32 == ch)))
+            {
+                result.pop_back();
+            }
+            else
+            {
+                result.push_back(ch);
+            }
+        }
+
+        return result;
+    }
+};
